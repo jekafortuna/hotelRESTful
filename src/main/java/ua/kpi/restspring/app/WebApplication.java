dd30390bcf3195d37c844102eaf1758fbd19e3c0
jekafortuna <evgeniy.fortuna@gmail.com>
2018-03-27 16:35:30 +0300

    separated all modules

---
+++ src/main/java/ua/kpi/restspring/app/WebApplication.java
@@ -0,0 +1,15 @@
+package ua.kpi.restspring.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Evgeniy on 27.03.2018.
+ */
+
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
